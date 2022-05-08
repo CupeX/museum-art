3fffc04480f227f517f1bcfd312b09a77380f28c
CupeX <dejan.dj@hotmail.com>
2022-05-08 12:53:33 +0200

    restructuring

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,27 +1,33 @@
 import React, { useEffect, useState } from "react";
-import Filter from "./Filter";
-import Search from "./Search";
-import Tree from "./Tree";
+import Filter from "../components/Filter";
+import Search from "../components/Search";
+import Tree from "../components/Tree";
 
 import GetCollection from "../services/GetCollection";
 import GetCollectionsById from "../services/GetCollectionById";
 import { Card, CardGroup } from "reactstrap";
-import ArtPreview from "./ArtPreview";
+import ArtPreview from "../components/ArtPreview";
 
 const Layout = () => {
   const [selectedFilter, setSelectedFilter] = useState("all");
   const [enteredSearch, setEnteredSearch] = useState("");
   const [artSelected, setArtSelected] = useState("");
+  const [filteredList, setFilteredList] = useState([]);
   const { collections, isLoaded } = GetCollection();
   const { collectionsById, isLoadedById } = GetCollectionsById();
-  const [filteredList, setFilteredList] = useState([]);
 
   useEffect(() => {
     if (isLoaded && isLoadedById) {
-      filteredByType(selectedFilter);
+      // filteredByType(selectedFilter);
       setFilteredList(collections.collection);
+      // setFilteredList(
+      //   collections.collection.filter((x) =>
+      //     console.log(x.collection.filter((y) => y.type === selectedFilter))
+      //   )
+      // );
     }
   }, [isLoaded, isLoadedById, selectedFilter]);
+  console.log("filtered list:", filteredList);
 
   const selectedFilterHandler = (filter) => {
     setSelectedFilter(filter);
@@ -37,14 +43,7 @@ const Layout = () => {
 
   // console.log(collections);
 
-  const filteredByType = (selectedFilter) => {
-    setFilteredList(
-      filteredList.filter((x) =>
-        console.log(x.collection.filter((y) => y.type === selectedFilter))
-      )
-    );
-  };
-  // console.log(filteredList);
+  // const filteredByType = (selectedFilter) => {};
 
   // const filteredByType = (selectedFilter) => {
   //   setFilteredList(
